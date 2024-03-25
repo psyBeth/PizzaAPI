d03f6db5c6e567ef1dff243ac54f02664d29f30e
Betul <betulkoru06@gmail.com>
2024-03-25 23:52:56 +0300

    order model

--- src/models/order.js
+++ src/models/order.js
@@ -2,3 +2,45 @@
 /* ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+const OrderSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    pizzaId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pizza',
+        required: true,
+    },
+
+    size: {
+        type: String,
+        trim: true,
+        required: true,
+        enum: ['Small', 'Medium', 'Large', 'XLarge']
+    },
+
+    quantity: {
+        type: Number,
+        default: 1
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+
+    amount: {
+        type: Number,
+        required: true
+    }
+
+}, {
+    collection: 'orders',
+    timestamps: true
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
