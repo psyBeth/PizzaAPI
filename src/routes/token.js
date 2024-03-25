0cf92d9709eb661dbc2d6b96969a4500bcbbfd09
Betul <betulkoru06@gmail.com>
2024-03-25 22:46:45 +0300

    token router

--- src/routes/token.js
+++ src/routes/token.js
@@ -2,6 +2,19 @@
 /* ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+const token = require('../controllers/token')
+
+// URL: /tokens
+
+router.route('/')
+    .get(token.list)
+    .post(token.create)
+
+router.route('/:id')
+    .get(token.read)
+    .put(token.update)
+    .patch(token.update)
+    .delete(token.delete)
 
 /* ------------------------------------------------------- */
 module.exports = router
