c1b7d27c4191d73a50dcb42b1073dfb33625d82c
Betul <betulkoru06@gmail.com>
2024-03-25 23:36:41 +0300

    topping router

--- src/controllers/topping.js
+++ src/controllers/topping.js
@@ -1,2 +1,19 @@
 "use strict"
 /* ------------------------------------------------------- */
+
+const topping = require('../controllers/topping');
+
+// URL: /toppings
+
+router.route('/')
+    .get(topping.list)
+    .post(topping.create)
+
+router.route('/:id')
+    .get(topping.read)
+    .put(topping.update)
+    .patch(topping.update)
+    .delete(topping.delete)
+
+/* ------------------------------------------------------- */
+module.exports = router;
