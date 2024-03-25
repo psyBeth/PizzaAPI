dee57f66269cef642d20e629ab820309b2670d0f
Betul <betulkoru06@gmail.com>
2024-03-25 22:44:13 +0300

    token model

--- src/models/token.js
+++ src/models/token.js
@@ -2,3 +2,28 @@
 /* ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const TokenSchema = new mongoose.Schema({
+    
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true,
+        index: true,
+    },
+
+    token: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true,
+    },
+
+} , {
+    collection: 'tokens',
+    timestamps: true,
+});
+
+module.exports = mongoose.model('Token', TokenSchema);
