124d67731b7783301de81475813525a7d4819649
Betul <betulkoru06@gmail.com>
2024-03-25 23:06:21 +0300

    auth router

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,13 @@
 /* ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+const auth = require('../controllers/auth')
 
+// URL: /auth
+
+// Login/logout:
+router.post('/login', auth.login)
+// router.all('/logout', auth.logout)
+router.get('/logout', auth.logout)
 /* ------------------------------------------------------- */
 module.exports = router
