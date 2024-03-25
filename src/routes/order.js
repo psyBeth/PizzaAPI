95e828c00773f414d3fe62cbbb1a81562e30ba23
Betul <betulkoru06@gmail.com>
2024-03-26 00:05:20 +0300

    order controller and router

--- src/routes/order.js
+++ src/routes/order.js
@@ -2,6 +2,19 @@
 /* ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+const order = require('../controllers/order')
+
+// URL: /orders
+
+router.route('/')
+    .get(order.list)
+    .post(order.create)
+
+router.route('/:id')
+    .get(order.read)
+    .put(order.update)
+    .patch(order.update)
+    .delete(order.delete)
 
 /* ------------------------------------------------------- */
 module.exports = router
