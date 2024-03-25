4d258078069ed770b1324b65ac29b5cc218f7c68
Betul <betulkoru06@gmail.com>
2024-03-26 00:14:22 +0300

    amout edited

--- src/models/order.js
+++ src/models/order.js
@@ -36,8 +36,9 @@ const OrderSchema = new mongoose.Schema({
     amount: {
         type: Number,
         // required: true
-        default: function() {return this.quantity * this.price},  // works only in create method 
-        transform: function() {return this.quantity * this.price},   // make it work in update too
+        // default: function() {return this.quantity * this.price},  // works only in create method 
+        // transform: function() {return this.quantity * this.price},   // make it work in update too
+        set: function() {return this.quantity * this.price}, // all conditions
     }
 
 }, {
