78823fcc8ba3b4f419df14aa89415de2d5598287
Betul <betulkoru06@gmail.com>
2024-03-25 22:37:29 +0300

    1st method is shorter

--- src/models/user.js
+++ src/models/user.js
@@ -23,23 +23,23 @@ const UserSchema = new mongoose.Schema({
         // ],
         // set: passwordEncrypt,
         //? 1ST METHOD
-        // set: (password) => {
-        //     if(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password)) {
-        //         return passwordEncrypt(password);
-        //     } else {
-        //         res.errorStatusCode = 403;
-        //         throw new Error('Password type is incorrect.');
-        //     }; 
-        // },
-        //? 2ND METHOD
         set: (password) => {
-            if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password)) {
-                return passwordEncrypt(password)
+            if(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password)) {
+                return passwordEncrypt(password);
             } else {
-                return 'wrong'
-            }
+                res.errorStatusCode = 403;
+                throw new Error('Password type is incorrect.');
+            }; 
         },
-        validate: (password) => (password != 'wrong'),
+        //? 2ND METHOD
+        // set: (password) => {
+        //     if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password)) {
+        //         return passwordEncrypt(password)
+        //     } else {
+        //         return 'wrong'
+        //     }
+        // },
+        // validate: (password) => (password != 'wrong'),
         // can also be done with middleware, but this is easier
         // in mongoose set function works before from validate function, thats why we are gonna make validation in set function
     },
