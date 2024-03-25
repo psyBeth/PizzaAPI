938704a5d235259af9665f7199c1d40bf55d4420
Betul <betulkoru06@gmail.com>
2024-03-25 21:35:00 +0300

    user model

--- src/models/user.js
+++ src/models/user.js
@@ -4,3 +4,41 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+//User model:
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+const UserSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        set: passwordEncrypt,
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+
+} , {
+    collection: "users",
+    timestamps: true,
+});
+
+module.exports = mongoose.model('User', UserSchema);
