9db11cf7e18b55a7214f49e430bba7bc7453fce4
Betul <betulkoru06@gmail.com>
2024-03-25 23:46:20 +0300

    pizza model controller router

--- src/routes/pizza.js
+++ src/routes/pizza.js
@@ -2,6 +2,19 @@
 /* ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+const pizza = require('../controllers/pizza')
+
+// URL: /pizzas
+
+router.route('/')
+    .get(pizza.list)
+    .post(pizza.create)
+
+router.route('/:id')
+    .get(pizza.read)
+    .put(pizza.update)
+    .patch(pizza.update)
+    .delete(pizza.delete)
 
 /* ------------------------------------------------------- */
 module.exports = router
