dce2ad83647a9c1b1621fa6edcc516752a2d1a96
Betul <betulkoru06@gmail.com>
2024-03-25 23:35:09 +0300

    topping model

--- src/models/topping.js
+++ src/models/topping.js
@@ -1,4 +1,20 @@
 "use strict"
 /* ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require('../configs/dbConnection');
 /* ------------------------------------------------------- */
+
+const ToppingSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    }
+
+}, {
+    collection: 'toppings',
+    timestamps: true
+});
+
+module.exports = mongoose.model('Topping', ToppingSchema);
