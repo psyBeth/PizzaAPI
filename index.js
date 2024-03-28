4f2fa3cea598c31860a37edd43518c956c9937dd
Betul <betulkoru06@gmail.com>
2024-03-28 21:58:24 +0300

    sent an actual mail to my gmail

--- index.js
+++ index.js
@@ -67,25 +67,45 @@ const nodemailer = require('nodemailer');
     }
 */
 
-const transporter = nodemailer.createTransport({
+//* Connect:
+// const transporter = nodemailer.createTransport({
     //SMTP:
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false,
+//     host: 'smtp.ethereal.email',
+//     port: 587,
+//     secure: false,
+//     auth: {
+//         user: 'fe4n7oqlepcl2tsb@ethereal.email',
+//         pass: 'jK4qAYD4qn4twpHtwY',
+//     }
+// });
+//* SendMail:
+// transporter.sendMail({
+//     from: 'fe4n7oqlepcl2tsb@ethereal.email',
+//     to: 'betulkoru06@gmail.com',
+//     subject: 'Hello',
+//     text: 'Hello there, how are you doing?',
+//     html: '<b>Hello there,</b><p>How are you doing?</p>'
+// }, (error, success) => {
+//     success ? console.log('SUCCESS', success) : console.log('ERROR', error);
+// })
+
+//* GoogleMail (gmail):
+//* Google -> AccountHome -> Security -> Two-Step-Verify -> App-Passwords
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
     auth: {
-        user: 'fe4n7oqlepcl2tsb@ethereal.email',
-        pass: 'jK4qAYD4qn4twpHtwY',
+        user: 'betulkoru06@gmail.com',
+        pass: 'mzpf ieyk kylc vaxk', // created an app password 
     }
 });
+//* SendMail:
 transporter.sendMail({
-    from: 'fe4n7oqlepcl2tsb@ethereal.email',
+    // from: 'betulkoru06@gmail.com',
     to: 'betulkoru06@gmail.com',
     subject: 'Hello',
     text: 'Hello there, how are you doing?',
     html: '<b>Hello there,</b><p>How are you doing?</p>'
-}, (error, success) => {
-    success ? console.log('SUCCESS', success) : console.log('ERROR', error);
-})
+}, (error, success) => console.log(success, error));
 
 /* ------------------------------------------------------- */
 // Routes:
